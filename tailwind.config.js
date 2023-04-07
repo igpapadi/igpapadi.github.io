e883ad0c891fb28bb4222131539c4d8876555c5e
Ilias Papadimitriou <43881975+igpapadi@users.noreply.github.com>
2023-04-07 14:36:13 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+      "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {backgroundImage:{
+      'potential':'./assets/background_image_2.jpg'
+    }},
+  },
+  plugins: [      
+    require('flowbite/plugin')
+],
+}
